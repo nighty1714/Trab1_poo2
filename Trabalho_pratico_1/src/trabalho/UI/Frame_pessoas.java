88c91bb9d580b68fcdaaa51f546291e027132e8a
G-Nozawa <gabriel.nozawa@unesp.br>
2022-01-11 15:11:54 -0300

    Retirado metodo redundante da interface

--- Trabalho_pratico_1/src/trabalho/UI/Frame_pessoas.java
+++ Trabalho_pratico_1/src/trabalho/UI/Frame_pessoas.java
@@ -169,7 +169,7 @@ public class Frame_pessoas extends javax.swing.JFrame {
         
         int data_mes = Integer.parseInt(Mes.getText());
         
-        controle.funcionario_do_mes(data_mes, data_ano);
+        controle.funcionarioDoMes(data_mes, data_ano);
             
         
         
